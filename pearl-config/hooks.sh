90b8853e4753e5adaf066106582c93960314ddac
Filippo Squillace <squillace.filippo@gmail.com>
2020-01-16 00:21:19 +0100

    Add package.conf and hooks.sh

---
+++ pearl-config/hooks.sh
@@ -0,0 +1,21 @@
+function post_install(){
+    local pluginname=gnupg
+    local giturl=https://github.com/jamessan/vim-gnupg.git
+
+    info "Installing or updating the $pluginname git repository..."
+    local plugin_root="${PEARL_PKGVARDIR}/plugins/pack/pearl/start"
+    mkdir -p "$plugin_root"
+    install_or_update_git_repo $giturl "$plugin_root/$pluginname" master
+    [[ -e "$plugin_root/$pluginname/doc" ]] && \
+        vim -c "helptags $plugin_root/$pluginname/doc" -c q
+
+    return 0
+}
+
+function post_update(){
+    post_install
+}
+
+function pre_remove(){
+    rm -rf "${PEARL_PKGVARDIR}/plugins"
+}
